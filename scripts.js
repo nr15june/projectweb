90d22f9a622b5e81bbae45981cd9c42ce0d3a9d1
nr15june <nureejehlong@gmail.com>
2024-09-09 20:27:25 +0700

    first commit

---
+++ scripts.js
@@ -0,0 +1,22 @@
+// function goBack() {
+//     window.history.back();
+// }
+
+// function confirm() {
+//     alert('Confirmed!');
+// }
+
+// function showTimePicker() {
+//     // แทนที่ด้วยการทำงานที่คุณต้องการ เช่น ใช้ไลบรารีสำหรับการเลือกเวลา
+//     alert('Time picker not implemented');
+// }
+
+// function showDatePicker(type) {
+//     // แทนที่ด้วยการทำงานที่คุณต้องการ เช่น ใช้ไลบรารีสำหรับการเลือกวันที่
+//     alert(`Date picker for ${type} not implemented`);
+// }
+// document.addEventListener('DOMContentLoaded', function() {
+//     flatpickr("#start-date", { dateFormat: "Y-m-d" });
+//     flatpickr("#end-date", { dateFormat: "Y-m-d" });
+//     flatpickr("#time", { enableTime: true, noCalendar: true, dateFormat: "H:i" });
+// });
